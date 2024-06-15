0535e34a13569c91a3206f0457f809816e5874c2
Nurillahi <n.softnhdev@gmail.com>
2024-06-15 11:25:21 +0800

    initial commit

--- nsaas/urls.py
+++ nsaas/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_page_view
+from .views import home_view, about_view
 
 urlpatterns = [
-    path("", home_page_view), #index page -> root page
-    path("Hello World/", home_page_view),
-    path("Hello World.html", home_page_view),
+    path("", home_view), #index page -> root page
+    path("about/", about_view),
+    path("Hello World/", home_view),
+    path("Hello World.html", home_view),
     path('admin/', admin.site.urls),
 ]
