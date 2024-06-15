71897750501fc3c75eebeaf89a06d755ea09e8cf
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2024-06-16 00:42:09 +0800

    Prepare for Railway

--- nsaas/views.py
+++ nsaas/views.py
@@ -12,7 +12,7 @@ def about_view(request, *args, **kwargs):
   qs = PageVisit.objects.all()
   page_qs = PageVisit.objects.filter(path=request.path)
   try:
-    percent = (page_qs.count() * 100.0) / qs.count(),
+    percent = (page_qs.count() * 100.0) / qs.count()
   except: 
     percent = 0
   my_title = "My Page"
